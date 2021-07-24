1859fd563efc1e4a2d68185227ef1cf79402d064
huynehehehe2212 <87896852+huynehehehe2212@users.noreply.github.com>
2021-07-24 17:23:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Anh đã yêu em ngay từ lần đầu tiên anh thấy nụ cười của em',
     desc: 'Và bắt đầu say em từ lúc đó ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    btnYes: 'Vẫn cứ là thích anh <3',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới có hơn 7 tỷ nụ cười nhưng mà với anh nụ cười em mới là đẹp nhất',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <3',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều 😘',
+    reply: 'Anh biết là em sẽ gõ "EM YÊU ANH" mà ( ͡° ͜ʖ ͡°)  ',
+    mess: 'Yêu em nhiều ',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Oke anh luôn <3',
     messLink: 'https://www.facebook.com/HHH.HHH.HHHUyy/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
